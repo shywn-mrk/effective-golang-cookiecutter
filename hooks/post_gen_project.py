fff360873d0b8ec047f433d1f3e6138387d92ba0
Shayan Karimi Nafchi <sh.kariminafchi@wallex.net>
2025-08-17 12:42:11 +0330

    Add pre-commit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,20 +1,24 @@
-from pathlib import Path
 import subprocess
+from pathlib import Path
+
 
 def remove_mysql_client():
     path = Path("internal", "clients", "mysql_client.go")
     if path.exists():
         path.unlink()
 
+
 def remove_postgresql_client():
     path = Path("internal", "clients", "postgresql_client.go")
     if path.exists():
         path.unlink()
 
+
 def remove_redis_client():
     path = Path("internal", "clients", "redis_client.go")
     if path.exists():
         path.unlink()
+
 
 def remove_database_clients():
     if "{{cookiecutter.database}}" == "mysql":
@@ -22,19 +26,22 @@ def remove_database_clients():
     elif "{{cookiecutter.database}}" == "postgresql":
         remove_mysql_client()
 
+
 def go_tidy():
-    print('Running go mod tidy...')
+    print("Running go mod tidy...")
     subprocess.run(["go", "mod", "tidy"], check=True)
 
+
 def build_compose():
-    print('Building Docker Compose files...')
+    print("Building Docker Compose files...")
     subprocess.run(["docker", "compose", "build"], check=True)
 
+
 def run():
-    print('Running post-generation hook...')
+    print("Running post-generation hook...")
 
     remove_database_clients()
-    
+
     if "{{cookiecutter.use_redis}}" == "no":
         remove_redis_client()
 
@@ -43,6 +50,7 @@ def run():
 
     if "{{cookiecutter.build_compose}}" == "yes":
         build_compose()
+
 
 if __name__ == "__main__":
     run()
